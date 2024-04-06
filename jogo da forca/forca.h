adf5996db31d541fefed8ccf44ff24803e94b58f
Daniel Arrudas de Macêdo <100447748+DanielArrudas@users.noreply.github.com>
2024-04-06 13:32:06 -0300

    Add files via upload

---
+++ jogo da forca/forca.h
@@ -0,0 +1,10 @@
+#define TAMANHO_PALAVRA 20
+void escolhepalavra();
+void abertura();
+void desenhaforca();
+int jachutou(char letra);
+void chuta();
+int ganhou();
+int enforcou();
+void adicionapalavra();
+int chuteserrados();
